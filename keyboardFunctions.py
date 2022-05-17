9573d307f5b239ff2b952d47c283f93080a251f7
Halmurat-Uyghur <halmurat.sdet@gmail.com>
2022-05-17 16:22:32 -0400

    keyboardFunctions added

---
+++ keyboardFunctions.py
@@ -0,0 +1,22 @@
+
+import pyautogui
+
+
+
+pyautogui.keyDown('shift')
+pyautogui.press('#')
+
+pyautogui.write("hello")
+pyautogui.press("space")
+pyautogui.write("world")
+pyautogui.press("enter")
+
+
+
+
+
+
+
+
+
+
